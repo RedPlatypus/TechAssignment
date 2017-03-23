f5f3c9b948988d1d366f74a0d6fbcd98c3074bc6
Brendan Robertson <brendanr90@gmail.com>
2017-03-23 09:10:15 -0700

    Updated peak/valley search pattern for correct answers. Pulled Transformer data into part2.js to avoid errorwith missing references to data. Added screenshot of results in console

--- part1.js
+++ part1.js
@@ -10,32 +10,173 @@ that Aequilibrium should build on that stretch of land?
 You can assume that you can always build a castle at the start of the array.
 */
 
-//my assumption on the question
-var land1 = [50, 200, 2000, 50, 500, 2000, 50]; // should be 3 valley + 2 peak = 5
-var land2 = [20,0,40,0,50,0] // should be 3 valley + 1 peak = 4
+function LandArray(){}; 
 
-var numOfCastels = function(landHeights){
-	var castels = i = 0,
-		minHeight = maxHeight = landHeights[0];
+LandArray.getPeaksAndValleys = function(heights = [], logging = false){
+	//if left & right are both lower || higher, we have a peak / valley
+	this.logging = logging;
+	this.state = 0; 			// -1 = falling, 0 = flat, 1 = rising
+	this.castles = 0; 		// count of castles
+	this.transition = true; 	// when true, we have gone through a state change that results in a peak or valley
+	this.height = this.pastHeight = heights[0];//initalize height
+	this.pastState = 0;		//we will need state
+	this.slopeState = 0; 	//we will need to check for doubles of (rising & falling) i.e. 4,4,3,3,2,2,1,1 should count for 1 valley not 3 valleys
 
-	//find min & max
-	for (i = 0; i < landHeights.length; i++) {
-		if(landHeights[i] < minHeight)
-			minHeight = landHeights[i];
+	for (let [index, height] of heights.entries()) {
+		// fancy way of getting 1 or -1 or 0 for state
+		this.height = height; //let everyone have current height.
+		this.state = this.height - this.pastHeight;
+		this.state = this.state > 0 ? 1 : this.state < 0 ? -1 : 0;
 
-		if(landHeights[i] > maxHeight)
-			maxHeight = landHeights[i];
+
+		//check last index. This method is based on looking backwards to tell us if we are on a peak or valley, so special check for last entry.
+		if (index === heights.length - 1) { 
+			this._checkLast();
+			//no continue, because we are still checking trend for last values
+		};
+
+		//if state check doesn't pan out, check the trend.
+		if(this._checkTrend()) {
+			this._setPastValues();
+			this._buildCastel();
+			continue;
+		}
+
+		//call appropriate method depending on current state
+		this.state > 0 ? this._rising() : this.state < 0 ? this._falling() : this._flat();
+		if(this.transition){
+			this._setPastValues();	
+			this._buildCastel();
+			continue;
+		}
+
+		this._setPastValues();
+
+	};
+	return this.castles;
+}
+
+LandArray._buildCastel = function(){
+	this.castles++;
+	if(this.logging) console.log("Increased castles: " + this.castles);
+	this.transition = false;
+}
+
+LandArray._falling = function(){
+	this.slopeState = -1;
+	if (this.pastState == -1) {
+		if(this.logging) console.log(`${this.height} Youre continuing to fall: pastState: ${this.pastState}, pastHeight: ${this.pastHeight}, currentState: ${this.state}, slopeState: ${this.slopeState}`);
+	} else if (this.pastState == 0) {
+		if(this.logging) console.log(`${this.height} Youve started to fall: pastState: ${this.pastState}, pastHeight: ${this.pastHeight}, currentState: ${this.state}, slopeState: ${this.slopeState}`);
+	} else if (this.pastState == 1) {
+		if(this.logging) console.log(`${this.height} Youve fall after rising: pastState: ${this.pastState}, pastHeight: ${this.pastHeight}, currentState: ${this.state}, slopeState: ${this.slopeState}`);
+		transition = true;
+	};
+}
+
+LandArray._flat = function(){
+	if (this.pastState == -1) {
+		if(this.logging) console.log(`${this.height} Youre flattening after a fall: pastState: ${this.pastState}, pastHeight: ${this.pastHeight}, currentState: ${this.state}, slopeState: ${this.slopeState}`);
+	} else if (this.pastState == 0) {
+		if(this.logging) console.log(`${this.height} Youre on a plain: pastState: ${this.pastState}, pastHeight: ${this.pastHeight}, currentState: ${this.state}, slopeState: ${this.slopeState}`);
+	} else if (this.pastState == 1) {
+		if(this.logging) console.log(`${this.height} Youre flattening after a rise: pastState: ${this.pastState}, pastHeight: ${this.pastHeight}, currentState: ${this.state}, slopeState: ${this.slopeState}`);
+	};
+}
+
+LandArray._rising = function(){
+	this.slopeState = 1;
+	if (this.pastState == -1) {
+		//you just came out of a valley
+		if(this.logging) console.log(`${this.height} Youve started to rise after falling: pastState: ${this.pastState}, pastHeight: ${this.pastHeight}, currentState: ${this.state}, slopeState: ${this.slopeState}`);
+		transition = true;
+	} else if (this.pastState == 0) {
+		if(this.logging) console.log(`${this.height} Youve started to rise: pastState: ${this.pastState}, pastHeight: ${this.pastHeight}, currentState: ${this.state}, slopeState: ${this.slopeState}`);
+	} else if (this.pastState == 1) {
+		if(this.logging) console.log(`${this.height} youre still rising: pastState: ${this.pastState}, pastHeight: ${this.pastHeight}, currentState: ${this.state}, slopeState: ${this.slopeState}`);
+	};
+}
+
+LandArray._checkTrend = function(){
+	let change = false
+	if (this.slopeState == -1 && this.state == 1) {
+		//was trending down.. now rising
+		this.transition = true;
+		this.slopeState = 1;
+		this.change = true;
+		if(this.logging) console.log(`${this.height} You just came from a large valley. Build there.: pastState: ${this.pastState}, pastHeight: ${this.pastHeight}, currentState: ${this.state}, slopeState: ${this.slopeState}`);
+		
+	} else if(this.slopeState == 1 && this.state == -1){
+		//was trending down.. now rising
+		this.transition = true;
+		this.slopeState = -1;
+		this.change = true;
+		if(this.logging) console.log(`${this.height} You just came off a large peak, build there.: pastState: ${this.pastState}, pastHeight: ${this.pastHeight}, currentState: ${this.state}, slopeState: ${this.slopeState}`);
 		
 	}
+	return change;
+}
+LandArray._setPastValues = function(){
+	this.pastState = this.state;	
+	this.pastHeight = this.height;
+}
 
-	// check for num of peaks/ valleys
-	for (i = 0; i < landHeights.length; i++) {
-		if (landHeights[i] == minHeight || landHeights[i] == maxHeight)
-			castels ++;
-	}
+LandArray._checkLast = function(){
+	//always returns true, unless you never moved up or down.
+	if (this.state !== 0 || this.slopeState !== 0 ) {
+		if(this.logging) console.log("Building Castel on final peak/valley");
+		this._buildCastel();
+	};
+}
 
-	return castels;
-};
 
-console.log("for land plot 1: " + numOfCastels(land1));
-console.log("for land plot 2: " + numOfCastels(land2));
+//pass in `true` as 2nd parameter to see this.logging output.
+//EX to get logging: LandArray.getPeaksAndValleys(landStretch, true);
+
+var landStretch = [2,2,3,4,3,3,2,2,1,1,2,5]; // should be 4
+console.log("[2,2,3,4,3,3,2,2,1,1,2,5] (4) returns : " + LandArray.getPeaksAndValleys(landStretch));
+
+var landStretch = [2,2,3,3,4,4,3,3,2,2,1,1,2,5,3,1,1,2]; // should be 6
+console.log("[2,2,3,3,4,4,3,3,2,2,1,1,2,5,3,1,1,2] (6) returns : " + LandArray.getPeaksAndValleys(landStretch));
+
+
+var landStretch = [2,2,2,3,3,3,4,4,4,4,3,3,3,3,2,2,2,2,1,1,1,2,2,2,5,5,5,3,3,3,1,1,2,2,2]; // should be 6
+console.log("[2,2,2,3,3,3,4,4,4,4,3,3,3,3,2,2,2,2,1,1,1,2,2,2,5,5,5,3,3,3,1,1,2,2,2] (6) returns : " + LandArray.getPeaksAndValleys(landStretch));
+
+
+var landStretch = [1,2,1,2]; // should be 4
+console.log("[1,2,1,2] (4) returns : " + LandArray.getPeaksAndValleys(landStretch));
+
+landStretch = [1,2,1,2,3]; // should be 4
+console.log("[1,2,1,2,3] (4) returns : " + LandArray.getPeaksAndValleys(landStretch));
+
+
+landStretch = [1,2,1,2,3,3]; // should be 3
+console.log("[1,2,1,2,3,3] (4) returns : " + LandArray.getPeaksAndValleys(landStretch));
+
+landStretch = [1,1,1,1,1,1]; // should be 3
+console.log("[1,1,1,1,1,1] (1) returns : " + LandArray.getPeaksAndValleys(landStretch));
+
+var landStretch = [3,2,1]; // should be 2
+console.log("[3,2,1] (2) returns : " + LandArray.getPeaksAndValleys(landStretch));
+
+var landStretch = [3,2,1,1,1]; // should be 2
+console.log("[3,2,1,1,1] (2) returns : " + LandArray.getPeaksAndValleys(landStretch));
+
+landStretch = [1,2,1,2,3,4]; // should be 4
+console.log("[1,2,1,2,3,4] (4) returns : " + LandArray.getPeaksAndValleys(landStretch));
+
+var landStretch = [1,2,1,2,3,40,3]; // should be 5
+console.log("[1,2,1,2,3,40,3] (5) returns : " + LandArray.getPeaksAndValleys(landStretch));
+
+var landStretch = [5,2,1,2,3,40,3]; // should be 5
+console.log("[5,2,1,2,3,40,3] (4) returns : " + LandArray.getPeaksAndValleys(landStretch));
+
+var landStretch = [2,2,1,2,3,40,3]; // should be 5
+console.log("[2,2,1,2,3,40,3] (4) returns : " + LandArray.getPeaksAndValleys(landStretch));
+
+
+
+
+
+
--- part2.js
+++ part2.js
@@ -3,6 +3,170 @@ console.log("Part 2");
 By: Brendan Robertson, March 17, 2017
 For: Aequilibrium
 */
+
+
+let transformers = [
+	{
+		name: "Pounce",
+		allegiance: "Rogue",
+		strength: 5,
+		intelligence: 7,
+		speed: 4,
+		endurance: 5,
+		rank: 6,
+		courage: 8,
+		firepower: 7,
+		skill: 10
+	},
+	{
+		name: "Sandstorm",
+		allegiance: "D",
+		strength: 7,
+		intelligence: 9,
+		speed: 6,
+		endurance: 7,
+		rank: 6,
+		courage: 10,
+		firepower: 6,
+		skill: 9
+	},
+	{
+		name: "Optimus Prime",
+		allegiance: "A",
+		strength: 10,
+		intelligence: 10,
+		speed: 8,
+		endurance: 10,
+		rank: 10,
+		courage: 10,
+		firepower: 8,
+		skill: 10
+	},
+	{
+		name: "Soundwave",
+		allegiance: "D",
+		strength: 8,
+		intelligence: 9,
+		speed: 2,
+		endurance: 6,
+		rank: 7,
+		courage: 5,
+		firepower: 6,
+		skill: 10
+	},
+	{
+		name: "Bluestreak",
+		allegiance: "A",
+		strength: 6,
+		intelligence: 6,
+		speed: 7,
+		endurance: 9,
+		rank: 5,
+		courage: 2,
+		firepower: 9,
+		skill: 7
+	},
+	{
+		name: "Hubcap",
+		allegiance: "A",
+		strength: 3,
+		intelligence: 9,
+		speed: 4,
+		endurance: 3,
+		rank: 6,
+		courage: 8,
+		firepower: 1,
+		skill: 9
+	},
+	{
+		name: "testa",
+		allegiance: "A",
+		strength: 11,
+		intelligence: 11,
+		speed: 11,
+		endurance: 11,
+		rank: 11,
+		courage: 11,
+		firepower: 11,
+		skill: 11
+	},
+	{
+		name: "testd",
+		allegiance: "D",
+		strength: 11,
+		intelligence: 11,
+		speed: 11,
+		endurance: 11,
+		rank: 11,
+		courage: 11,
+		firepower: 11,
+		skill: 11
+	},
+	{
+		name: "Dr. Probious",
+		allegiance: "D",
+		strength: 9,
+		intelligence: 4,
+		speed: 8,
+		endurance: 10,
+		rank: 8,
+		courage: 10,
+		firepower: 8,
+		skill: 10
+	},{
+		name: "Micilan Man",
+		allegiance: "A",
+		strength: 10,
+		intelligence: 10,
+		speed: 8,
+		endurance: 10,
+		rank: 2,
+		courage: 10,
+		firepower: 8,
+		skill: 10
+	}
+];
+
+let transformers2 = [
+	{
+		name: "Soundwave",
+		allegiance: "D",
+		strength: 8,
+		intelligence: 9,
+		speed: 2,
+		endurance: 6,
+		rank: 7,
+		courage: 5,
+		firepower: 6,
+		skill: 10
+	},
+	{
+		name: "Bluestreak",
+		allegiance: "A",
+		strength: 6,
+		intelligence: 6,
+		speed: 7,
+		endurance: 9,
+		rank: 5,
+		courage: 2,
+		firepower: 9,
+		skill: 7
+	},
+	{
+		name: "Hubcap",
+		allegiance: "A",
+		strength: 4,
+		intelligence: 4,
+		speed: 4,
+		endurance: 4,
+		rank: 4,
+		courage: 4,
+		firepower: 4,
+		skill: 4
+	}
+];
+
+
 Array.prototype.contains = function(object){
 	for (i = 0; i < this.length; i++) {
         if (this[i] == object) 
@@ -283,6 +447,8 @@ class WarZone {
 	}
 };
 
+
+console.log("--Battle 1--");
 // Call runBattles from wherever on the warzone
 var battle = new WarZone(transformers2);
 // var battle = new WarZone(transformers); // run with a different group of transformers.
@@ -293,4 +459,11 @@ battle.runBattles();				// runs the battles
 // battle.reportWinner();		// report winner (based on most enemy team destroyed)
 // battle.reportLoser();		// report loser (based on most team mates destroyed)
 battle.reportScore();		// report winning team and survivors, losing team and survivors
+
+
+console.log("--Battle 2--");
+var battle2 = new WarZone(transformers);
+// battle2.toggleCommentary();	// gets active reports of battle2.
+battle2.runBattles();				// runs the battle2s
+battle2.reportScore();
 
